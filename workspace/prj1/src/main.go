5d360002a4192d53e4e80fe224959c1e84c28b9c
Saurabh Sharma <saurabh777@gmail.com>
2019-10-31 18:40:06 +0530

    first commit

---
+++ workspace/prj1/src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	// var x int = 200
+	// type Num int
+	// type Celsius float64
+
+	// var y Celsius
+
+	y := 100.230
+
+	// z := 300.20
+	x := "Hello, world!"
+
+	fmt.Println(" -------------- ")
+	fmt.Println("%s : x: %f ", x, y)
+	fmt.Println(" -------------- ")
+}
